27893706aca8afe345cbbfdc8d01f1fd3627fd6c
dishantsethi <dishantsethi14@gmail.com>
2023-08-09 19:13:48 +0530

    add support for download function

---
+++ willisapi_client/services/download/download_utils.py
@@ -0,0 +1,17 @@
+import requests
+import json
+import time
+import random
+
+class DownloadUtils:
+    def request(url, data, headers, try_number):
+        try:
+            response = requests.post(url, json=data, headers=headers)
+            res_json = response.json()
+        except (requests.exceptions.ConnectionError, json.decoder.JSONDecodeError) as ex:
+            if try_number == 3:
+                raise 
+            time.sleep(random.random()*2)
+            return DownloadUtils.request(url, data, headers, try_number=try_number+1)
+        else:
+            return res_json
