58b2ba385e2a54c5c2e2443f1ad322a21fff3992
dishantsethi <dishantsethi14@gmail.com>
2023-08-09 19:13:48 +0530

    add support for download function

--- willisapi_client/services/download/download_handler.py
+++ willisapi_client/services/download/download_handler.py
@@ -1,6 +1,9 @@
 # website:   https://www.brooklyn.health
 import pandas as pd
 
+from willisapi_client.willisapi_client import WillisapiClient
+from willisapi_client.services.download.download_utils import DownloadUtils
+
 def download(key, project_name = 'project_name'):
     """
     ---------------------------------------------------------------------------------------------------
@@ -22,5 +25,10 @@ def download(key, project_name = 'project_name'):
     ---------------------------------------------------------------------------------------------------
     """
 
-    c = 6 + 5
-    return c
+    wc = WillisapiClient()
+    url = wc.get_download_url()
+    headers = wc.get_headers()
+    headers['Authorization'] = key
+    data = dict(project_name=project_name)
+    response = DownloadUtils.request(url, data, headers, try_number=1)
+    return response
