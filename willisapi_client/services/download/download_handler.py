25732f4d796490fd2bb092ea19ef76cd547a54fa
dishantsethi <dishantsethi14@gmail.com>
2023-08-16 10:56:52 +0530

    patch download function

--- willisapi_client/services/download/download_handler.py
+++ willisapi_client/services/download/download_handler.py
@@ -1,10 +1,11 @@
 # website:   https://www.brooklyn.health
 import pandas as pd
+from http import HTTPStatus
 
 from willisapi_client.willisapi_client import WillisapiClient
 from willisapi_client.services.download.download_utils import DownloadUtils
 
-def download(key, project_name = 'project_name'):
+def download(key, project_name):
     """
     ---------------------------------------------------------------------------------------------------
 
@@ -31,4 +32,8 @@ def download(key, project_name = 'project_name'):
     headers['Authorization'] = key
     data = dict(project_name=project_name)
     response = DownloadUtils.request(url, data, headers, try_number=1)
-    return response
+    if 'message' in response and response['message'] == 'Unauthorized':
+        print("Your Key is expired. Login again to generate a new key")      
+    if response and 'status_code' in response and response['status_code'] == HTTPStatus.OK:
+        return response['items']    
+    return None
