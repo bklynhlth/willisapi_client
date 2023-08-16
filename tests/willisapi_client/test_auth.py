9e6da3a952c8ba822ded48056bc00121ba2472a7
dishantsethi <dishantsethi14@gmail.com>
2023-08-16 17:55:51 +0530

    setup testcase structure

---
+++ tests/willisapi_client/test_auth.py
@@ -0,0 +1,10 @@
+import pytest
+from unittest.mock import MagicMock, patch
+
+
+class TestLoginFunction:
+    def setup_method(self):
+        pass
+
+    def test_login_success(self):
+        assert 1 == 1
