d12e53b0458b83dc2ffefb9cb7fb2069530baedc
dishantsethi <dishantsethi14@gmail.com>
2023-08-16 18:33:39 +0530

    add doc str to every function

--- willisapi_client/services/upload/upload_utils.py
+++ willisapi_client/services/upload/upload_utils.py
@@ -7,6 +7,7 @@ import asyncio
 import aiohttp
 import nest_asyncio
 import json
+from typing import Tuple
 
 from willisapi_client.services.upload.utils import MB
 from willisapi_client.logging_setup import logger as logger
@@ -17,24 +18,59 @@ MAX_NUMBER_OF_PARTS = 10000
 PART_SIZE = 40 * MB
 
 class UploadUtils:
-    def _get_semaphore():
+    def _get_semaphore() -> int:
+        """
+        This function returns the semaphone count for async upload
+
+        Returns:
+            int
+        """
         return asyncio.Semaphore(os.cpu_count() - 2)
     
     @staticmethod
     def read_file(file_path: str):
+        """
+        This function reads the video/audio file and returns it in a binary format
+        """
         return open(file_path, 'rb')
     
     @staticmethod
     def close_file(file):
+        """
+        This function closes the video/audio file
+        
+        Parameter:
+            opened file in binary format
+        """
         file.close()
 
     @staticmethod
-    def get_file_size_and_parts(file_path):
+    def get_file_size_and_parts(file_path: str) -> int:
+        """
+        This function takes the file path and returns the number of presigned urls required to upload the file to Brooklyn health S3 bucket
+        
+        Parameter:
+            file_path
+
+        Returns:
+            int
+        """
         total_bytes = os.stat(file_path).st_size
         number_of_parts = math.ceil(total_bytes / PART_SIZE)
         return number_of_parts
     
-    def initiate_multi_part_upload(row, url, headers):
+    def initiate_multi_part_upload(row, url: str, headers: dict) -> Tuple[str, str]:
+        """
+        This is an internal function which makes a POST API call to brooklyn.health API server to initiate multi part upload
+
+        Parameters:
+            row: a dataframe row
+            url: API URL
+            headers: headers
+
+        Returns:
+            upload_id and record_id
+        """
         data = dict(project_name=row.project_name, workflow_tags=row.workflow_tags, pt_id_external=row.pt_id_external, filename=pathlib.Path(row.file_path).name)
         if row.time_collected:
             data["time_collected"] = row.time_collected
@@ -58,6 +94,20 @@ class UploadUtils:
             return (None, None)
     
     def fetch_pre_signed_part_urls(url: str, record_id: str, upload_id: str, file_path: str, headers):
+        """
+        This is an internal function which makes a POST API call to brooklyn.health API server to get all the presigned URLs at once
+
+        Parameters:
+            url: API URL
+            record_id: str
+            upload_id: str
+            file_path: str
+            headers: headers
+
+        Returns:
+            list of presigned urls: list
+            number of parts: int
+        """
         try:
             number_of_parts = UploadUtils.get_file_size_and_parts(file_path)
             url_to_get_presigned_url = f"{url}?type=presigned&record_id={record_id}&upload_id={upload_id}&number_of_parts={number_of_parts}"
@@ -75,6 +125,9 @@ class UploadUtils:
             return None, number_of_parts
 
     async def _async_upload(session, part_no, presigned_url, data, PARTS):
+        """
+        This is an async function which makes a PUT call to presigned URL
+        """
         try:
             async with session.put(presigned_url, data=data) as response:
                 ETag = response.headers["ETag"].strip('"')
@@ -83,6 +136,9 @@ class UploadUtils:
             pass
 
     async def async_upload(presigned_urls, file):
+        """
+        This is a internal function responsible for scheduling all the coroutines
+        """
         PARTS = []
         tasks = []
         async with UploadUtils._get_semaphore():
@@ -95,7 +151,14 @@ class UploadUtils:
         return SORTED_PARTS
 
     @staticmethod
-    def upload(row, url, headers):
+    def upload(row, url, headers) -> bool:
+        """
+        This is an internal funciton which first call the initiate_multipart_upload function, then upload the file in async way
+        and finally make a POST call to brooklyn.health API server to make the upload as complete/abort
+        
+        Returns:
+            uploaded: boolean
+        """
         (upload_id, record_id) = UploadUtils.initiate_multi_part_upload(row, url, headers)
         uploaded = False
         if upload_id and record_id:
@@ -117,6 +180,12 @@ class UploadUtils:
         return uploaded
     
     def start_upload(presigned_urls, file):
+        """
+        This is an internal function which executes the async function responsible for uploading file asynchronously
+
+        Returns:
+            parts: List of Etags response from AWS S3 
+        """
         try:
             loop = asyncio.get_running_loop()
         except RuntimeError:  # 'RuntimeError: There is no current event loop...'
@@ -131,6 +200,9 @@ class UploadUtils:
         return parts
 
     def summary_logs(number_of_files_uploaded: int, number_of_files_failed: int):
+        """
+        A function which logs the summary for upload
+        """
         logger.info("---------------------------------------")
         logger.info("upload summary")
         logger.info("---------------------------------------")
