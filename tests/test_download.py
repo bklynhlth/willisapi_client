62bc3257ba3397a4f36bf4c4a1e0a1611205aa69
dishantsethi <dishantsethi14@gmail.com>
2023-08-21 19:16:04 +0530

    add download tests success case

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,5 +1,6 @@
 from unittest.mock import patch
 import pandas as pd
+import json
 
 from willisapi_client.services.download.download_handler import download
 
@@ -49,36 +50,14 @@ class TestDownloadFunction:
 
     @patch('willisapi_client.services.download.download_utils.DownloadUtils.request')
     def test_download_success(self, mocked_data):
-        mocked_data = {}
-        # mocked_data.return_value = {
-        #     "status_code": 200,
-        #     "items": {
-        #         "project": [
-        #             {
-        #                 "project_Id": "id",
-        #                 "project_name": "dev_testing",
-        #                 "participant": [
-        #                     {
-        #                         "participant_Id": "pt_id",
-        #                         "results": [
-        #                             {
-        #                                 "file_name": "video.mp4",
-        #                                 "file_type": None,
-        #                                 "timestamp": None,
-        #                                 "measures":{
-        #                                     "emotional_expressivity_summary": [{'measure1': [1,2]}],
-        #                                     "facial_expressivity_summary": [{'measure2': [1,2]}],
-        #                                     "vocal_acoustics_summary": [{'measure3': [1,2]}],
-        #                                     "speech_characteristics_summary": [{'measure4': [1,2]}],
-        #                                 }
-        #                             }
-        #                         ]
-        #                     }
-        #                 ]
-        #             }
-        #         ]
-        #     },
-        # }
+        with open('tests/test.json') as json_file:
+            data = json.load(json_file)
+        mocked_data.return_value = data
         data = download(self.key, self.project_name)
-        assert data.empty == True
+        assert data.empty == False
+        assert data.filename.tolist()[0] == 'test_video.mp4'
+        assert data.pt_id_external.tolist()[0] == 'pt_id_external'
+        assert len(data.index) == 1
+
+
     
