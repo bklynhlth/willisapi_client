66300f7d4f9a76b98cd567e2aec772df084a4c2d
dishantsethi <dishantsethi14@gmail.com>
2023-08-29 19:13:11 +0530

    Add first_name and last_name args for signup function

--- willisapi_client/services/auth/user_manager.py
+++ willisapi_client/services/auth/user_manager.py
@@ -5,7 +5,7 @@ from willisapi_client.willisapi_client import WillisapiClient
 from willisapi_client.services.auth.auth_utils import AuthUtils
 from willisapi_client.logging_setup import logger as logger
 
-def create_user(key: str, client_email: str , client_name: str) -> str:
+def create_user(key: str, client_email: str , client_name: str, first_name: str, last_name: str) -> str:
     """
     ---------------------------------------------------------------------------------------------------
     Function: create_user
@@ -17,6 +17,8 @@ def create_user(key: str, client_email: str , client_name: str) -> str:
     key: Admin access token
     client_email: string representation of client email id
     client_name: stringbrepresentation of expected client name without empty spaces
+    first_name: User's first name
+    last_name: User's last_name
 
     Returns:
     ----------
@@ -28,7 +30,7 @@ def create_user(key: str, client_email: str , client_name: str) -> str:
     url = wc.get_signup_url()
     headers = wc.get_headers()
     headers['Authorization'] = key
-    data = dict(client_email=client_email, client_name=client_name)
+    data = dict(client_email=client_email, client_name=client_name, first_name=first_name, last_name=last_name)
     response = AuthUtils.signup(url, data, headers, try_number=1)
     if response and 'status_code' in response and response['status_code'] == HTTPStatus.OK:
         logger.info(f"Signup Successful for client: {client_name}, client_email: {client_email}")
