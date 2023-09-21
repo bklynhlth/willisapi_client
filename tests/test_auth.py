2cbe8e51d14c895836cd8da5e400efc2ff0704fc
bindritu1211 <kumariritubind@gmail.com>
2023-09-21 11:49:49 +0530

    update testcase for return datetime with expiretime

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,77 +1,65 @@
 from willisapi_client.services.auth.login_manager import login
 from willisapi_client.services.auth.user_manager import create_user
-from unittest.mock import Mock, patch
+from unittest.mock import patch
 from datetime import timedelta, datetime
-import pytest
+
 
 class TestLoginFunction:
     def setup_method(self):
+        self.dt = datetime.now()
         self.username = "dummy"
         self.password = "password"
         self.id_token = "dummy_token"
-        self.expires_in = datetime.now() + timedelta(0.02)
+        self.expires_in = 100
+        self.expires_in_date = str(
+            self.dt.replace(hour=0, minute=0, second=0, microsecond=0)
+            + timedelta(seconds=self.expires_in)
+        )
 
-    @patch('willisapi_client.services.auth.login_manager.AuthUtils.login')
+    @patch("willisapi_client.services.auth.login_manager.AuthUtils.login")
     def test_login_failed(self, mocked_login):
         mocked_login.return_value = {}
         key, expire_in = login("", "")
         assert key == None
         assert expire_in == None
 
-    @patch('willisapi_client.services.auth.login_manager.AuthUtils.login')
-    @pytest.mark.parametrize("current_time", [datetime(2023, 9, 20, 12, 0, 0)])
-    def test_login_success(self, mocked_login,current_time):
+    @patch("willisapi_client.services.auth.login_manager.AuthUtils.login")
+    def test_login_success(self, mocked_login):
         mocked_login.return_value = {
             "status_code": 200,
-            "result":{
-                "id_token": self.id_token,
-                "expires_in": self.expires_in
-            }
+            "result": {"id_token": self.id_token, "expires_in": self.expires_in},
         }
+
         key, expire_in = login(self.username, self.password)
-        with patch('datetime.datetime') as mock_datetime:
-            mock_datetime.now.return_value = current_time
+        expire_in = str(self.dt.replace(hour=0, minute=1, second=40, microsecond=0))
+        assert key == self.id_token
+        assert expire_in == self.expires_in_date
 
-            result = login()
-            assert key == self.id_token
-            # assert expire_in == self.expires_in
-            assert result == current_time
-
-
-    @patch('willisapi_client.services.auth.login_manager.AuthUtils.login')
+    @patch("willisapi_client.services.auth.login_manager.AuthUtils.login")
     def test_login_400_status(self, mocked_login):
         mocked_login.return_value = {
             "status_code": 400,
-            "result":{
-                "id_token": self.id_token,
-                "expires_in": self.expires_in
-            }
+            "result": {"id_token": self.id_token, "expires_in": self.expires_in},
         }
         key, expire_in = login(self.username, self.password)
         assert key == None
         assert expire_in == None
 
-    @patch('willisapi_client.services.auth.login_manager.AuthUtils.login')
+    @patch("willisapi_client.services.auth.login_manager.AuthUtils.login")
     def test_login_403_status(self, mocked_login):
         mocked_login.return_value = {
             "status_code": 403,
-            "result":{
-                "id_token": self.id_token,
-                "expires_in": self.expires_in
-            }
+            "result": {"id_token": self.id_token, "expires_in": self.expires_in},
         }
         key, expire_in = login(self.username, self.password)
         assert key == None
         assert expire_in == None
 
-    @patch('willisapi_client.services.auth.login_manager.AuthUtils.login')
+    @patch("willisapi_client.services.auth.login_manager.AuthUtils.login")
     def test_login_500_status(self, mocked_login):
         mocked_login.return_value = {
             "status_code": 500,
-            "result":{
-                "id_token": self.id_token,
-                "expires_in": self.expires_in
-            }
+            "result": {"id_token": self.id_token, "expires_in": self.expires_in},
         }
         key, expire_in = login(self.username, self.password)
         assert key == None
@@ -89,52 +77,85 @@ class TestSignupFunction:
         self.first_name = "First"
         self.last_name = "Last"
 
-    @patch('willisapi_client.services.auth.login_manager.AuthUtils.signup')
+    @patch("willisapi_client.services.auth.login_manager.AuthUtils.signup")
     def test_signup_failed(self, mocked_signup):
         mocked_signup.return_value = {
             "status_code": 200,
-            "message": "Not an admin user"
-            }
-        message = create_user(self.non_admin_key, self.client_email, self.client_name, self.first_name, self.last_name)
+            "message": "Not an admin user",
+        }
+        message = create_user(
+            self.non_admin_key,
+            self.client_email,
+            self.client_name,
+            self.first_name,
+            self.last_name,
+        )
         assert message == "Not an admin user"
 
-    @patch('willisapi_client.services.auth.login_manager.AuthUtils.signup')
+    @patch("willisapi_client.services.auth.login_manager.AuthUtils.signup")
     def test_signup_success(self, mocked_signup):
-        mocked_signup.return_value = {
-            "status_code": 200,
-            "message": "User created"
-            }
-        message = create_user(self.admin_key, self.client_email, self.client_name, self.first_name, self.last_name)
+        mocked_signup.return_value = {"status_code": 200, "message": "User created"}
+        message = create_user(
+            self.admin_key,
+            self.client_email,
+            self.client_name,
+            self.first_name,
+            self.last_name,
+        )
         assert message == "User created"
 
-    @patch('willisapi_client.services.auth.login_manager.AuthUtils.signup')
+    @patch("willisapi_client.services.auth.login_manager.AuthUtils.signup")
     def test_signup_failed_400_status(self, mocked_signup):
         mocked_signup.return_value = {
             "status_code": 400,
-            }
-        message = create_user(self.admin_key, self.client_email, self.client_name, self.first_name, self.last_name)
+        }
+        message = create_user(
+            self.admin_key,
+            self.client_email,
+            self.client_name,
+            self.first_name,
+            self.last_name,
+        )
         assert message == None
 
-    @patch('willisapi_client.services.auth.login_manager.AuthUtils.signup')
+    @patch("willisapi_client.services.auth.login_manager.AuthUtils.signup")
     def test_signup_failed_500_status(self, mocked_signup):
         mocked_signup.return_value = {
             "status_code": 500,
-            }
-        message = create_user(self.admin_key, self.client_email, self.client_name, self.first_name, self.last_name)
+        }
+        message = create_user(
+            self.admin_key,
+            self.client_email,
+            self.client_name,
+            self.first_name,
+            self.last_name,
+        )
         assert message == None
 
-    @patch('willisapi_client.services.auth.login_manager.AuthUtils.signup')
+    @patch("willisapi_client.services.auth.login_manager.AuthUtils.signup")
     def test_signup_failed_401_status(self, mocked_signup):
         mocked_signup.return_value = {
             "status_code": 401,
-            }
-        message = create_user(self.admin_key, self.client_email, self.client_name, self.first_name, self.last_name)
+        }
+        message = create_user(
+            self.admin_key,
+            self.client_email,
+            self.client_name,
+            self.first_name,
+            self.last_name,
+        )
         assert message == None
 
-    @patch('willisapi_client.services.auth.login_manager.AuthUtils.signup')
+    @patch("willisapi_client.services.auth.login_manager.AuthUtils.signup")
     def test_signup_failed_403_status(self, mocked_signup):
         mocked_signup.return_value = {
             "status_code": 403,
-            }
-        message = create_user(self.admin_key, self.client_email, self.client_name, self.first_name, self.last_name)
+        }
+        message = create_user(
+            self.admin_key,
+            self.client_email,
+            self.client_name,
+            self.first_name,
+            self.last_name,
+        )
         assert message == None
