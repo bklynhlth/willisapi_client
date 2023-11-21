c24bdcce053d54116b96d23b5b72520ef1b2efbc
bind-ritu1211 <kumariritubind@gmail.com>
2023-11-21 14:48:17 +0530

    add create account function and testcase

--- willisapi_client/__init__.py
+++ willisapi_client/__init__.py
@@ -1,6 +1,12 @@
 # website:   https://www.brooklyn.health
 
 # import the required packages
-from willisapi_client.services.api import login, create_user, upload, download
+from willisapi_client.services.api import (
+    login,
+    create_user,
+    upload,
+    download,
+    create_account,
+)
 
-__all__ = ["login", "create_user", "upload", "download"]
+__all__ = ["login", "create_user", "upload", "download", "create_account"]
