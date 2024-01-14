7bf5abfb9e7011432b916f247ca6f20fc1fd001b
dishantsethi <dishantsethi14@gmail.com>
2024-01-14 18:44:08 +0530

    remove create user support

--- willisapi_client/__init__.py
+++ willisapi_client/__init__.py
@@ -3,10 +3,9 @@
 # import the required packages
 from willisapi_client.services.api import (
     login,
-    create_user,
     upload,
     download,
     create_account,
 )
 
-__all__ = ["login", "create_user", "upload", "download", "create_account"]
+__all__ = ["login", "upload", "download", "create_account"]
