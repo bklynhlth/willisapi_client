87f5370012bd5800c55acf42b313afc7ec535a28
dishantsethi <dishantsethi14@gmail.com>
2024-02-21 13:12:58 +0530

    add user_permissions function

--- willisapi_client/services/auth/__init__.py
+++ willisapi_client/services/auth/__init__.py
@@ -2,5 +2,6 @@
 from willisapi_client.services.auth.login_manager import (
     login,
 )
+from willisapi_client.services.auth.permissions_manager import user_permissions
 
-__all__ = ["login",]
+__all__ = ["login", "user_permissions"]
