cecb4bef5cb68c393e6f15f53aa46fa76a43b9da
dishantsethi <dishantsethi14@gmail.com>
2026-06-12 14:37:22 +0530

    update baseurl

--- willisapi_client/willisapi_client.py
+++ willisapi_client/willisapi_client.py
@@ -14,18 +14,19 @@ class WillisapiClient:
     def __init__(self, *args, **kwargs) -> None:
         self.client_version = get_client_version()
         self.api_version = math.floor(self.client_version)
-        self.api_uri = "api.brooklyn.health"
+        self.api_uri = "api.willis.health"
         self.env = kwargs["env"] if "env" in kwargs else None
 
-    def get_base_url(self):
+    def get_api_host(self):
         if self.env:
-            return f"https://{self.env}-{self.api_uri}/v{self.api_version}/"
-        return f"https://{self.api_uri}/v{self.api_version}/"
+            return f"api.{self.env}.willis.health"
+        return self.api_uri
+
+    def get_base_url(self):
+        return f"https://{self.get_api_host()}/v{self.api_version}/"
 
     def get_base_v2_url(self):
-        if self.env:
-            return f"https://{self.env}-{self.api_uri}/api/v2/"
-        return f"https://{self.api_uri}/api/v2/"
+        return f"https://{self.get_api_host()}/api/v2/"
 
     def get_diarize_remaining_calls_url(self):
         return self.get_base_url() + "willis-diarize-call-remaining"
@@ -35,7 +36,7 @@ class WillisapiClient:
 
     def get_upload_url(self):
         return self.get_base_v2_url() + "metadata/upload"
-    
+
     def get_processed_upload_url(self):
         return self.get_base_v2_url() + "metadata/processed-data/upload"
 
